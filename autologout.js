d587b2cb246f9d0d3cdcc25c9391a85f8352d094
John Ennew <johne@deeson.co.uk>
2013-05-09 12:38:05 +0100

    Issue #1990308: Added fixes for #1114846 (display a message after auto logout), #1024508 (prevent logout when multiple tabs are open), #1989798 (Add a hook to allow other modules to set a page not to include the autologout js). Also some DCS cleanup

--- autologout.js
+++ autologout.js
@@ -2,16 +2,66 @@
   Drupal.behaviors.autologout = {
     attach: function(context, settings) {
 
-      var t = setTimeout(init, Drupal.settings.autologout.timeout);
       var paddingTimer;
+      var t;
+      var theDialog;
+
+      if (context == document) {
+        if (Drupal.settings.autologout.refresh_only) {
+          // On pages that cannot be logged out of don't start the logout countdown.
+          t = setTimeout(keepAlive, Drupal.settings.autologout.timeout);
+        }
+        else {
+          // On pages where the user can be logged out, set the timer to popup
+          // and log them out.
+          t = setTimeout(init, Drupal.settings.autologout.timeout);
+        }
+      }
 
       function init() {
-        dialog();
-        paddingTimer = setTimeout(logout, Drupal.settings.autologout.timeout_padding);
+        paddingTimer = setTimeout(confirmLogout, Drupal.settings.autologout.timeout_padding);
+
+        // While the countdown timer is going, lookup the remaining time. If there
+        // is more time remaining (i.e. a user is navigating in another tab), then
+        // reset the timer for opening the dialog.
+        $.ajax({
+          url : Drupal.settings.basePath + 'autologout_ajax_get_time_left',
+          dataType: 'json',
+          success: function(data) {
+            if (data.time > 0) {
+              clearTimeout(paddingTimer);
+              t = setTimeout(init, data.time);
+            }
+            else {
+              theDialog = dialog();
+            }
+          },
+          error: function(XMLHttpRequest, textStatus) {
+            if (XMLHttpRequest.status == 403) {
+              window.location = Drupal.settings.autologout.redirect_url;
+            }
+          }
+        });
+      }
+
+      function keepAlive() {
+        $.ajax({
+          url: Drupal.settings.basePath + "autologout_ahah_set_last",
+          type: "POST",
+          success: function() {
+            // After keeping the connection alive, start the timer again.
+            t = setTimeout(keepAlive, Drupal.settings.autologout.timeout);
+          },
+          error: function(XMLHttpRequest, textStatus) {
+            if (XMLHttpRequest.status == 403) {
+              window.location = Drupal.settings.autologout.redirect_url;
+            }
+          },
+        });
       }
 
       function dialog() {
-        $("<div> " +  Drupal.settings.autologout.message + "</div>").dialog({
+        return $('<div> ' +  Drupal.settings.autologout.message + '</div>').dialog({
           modal: true,
           closeOnEscape: false,
           width: "auto",
@@ -33,6 +83,25 @@
         });
       }
 
+     // A user could have used the reset button on the tab/window they're actively
+     // using, so we need to double check before actually logging out.
+     function confirmLogout() {
+       $(theDialog).dialog('destroy');
+       $.ajax({
+         url : Drupal.settings.basePath + 'autologout_ajax_get_time_left',
+         dataType: 'json',
+         error: logout,
+         success: function(data) {
+           if (data.time > 0) {
+             t = setTimeout(init, data.time);
+           }
+           else {
+             logout();
+           }
+         }
+       });
+     }
+
       function logout() {
         $.ajax({
           url: Drupal.settings.basePath + "autologout_ahah_logout",
@@ -41,7 +110,9 @@
             window.location = Drupal.settings.autologout.redirect_url;
           },
           error: function(XMLHttpRequest, textStatus) {
-           alert("There has been an error resetting your last access time: " + textStatus + ".")
+            if (XMLHttpRequest.status == 403) {
+              window.location = Drupal.settings.autologout.redirect_url;
+            }
           },
         });
       }
@@ -54,7 +125,9 @@
             t = setTimeout(init, Drupal.settings.autologout.timeout);
           },
           error: function(XMLHttpRequest, textStatus) {
-            alert("There has been an error resetting your last access time: " + textStatus + ".")
+            if (XMLHttpRequest.status == 403) {
+              window.location = Drupal.settings.autologout.redirect_url;
+            }
           },
         });
       }
