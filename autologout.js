7f5e09919ec9b980338053a0514c583b5b3cb92c
John Ennew <johne@deeson.co.uk>
2013-05-15 14:02:04 +0100

    Issue #1914156 by ceng: Reset countdown when a user types so content being created isnt lost

--- autologout.js
+++ autologout.js
@@ -6,12 +6,44 @@
       var t;
       var theDialog;
 
+      // Activity is a boolean used to detect a user has
+      // interacted with the page.
+      var activity;
+
       if (context == document) {
+
         if (Drupal.settings.autologout.refresh_only) {
           // On pages that cannot be logged out of don't start the logout countdown.
           t = setTimeout(keepAlive, Drupal.settings.autologout.timeout);
         }
         else {
+          // Set no activity to start with.
+          activity = false;
+
+          // Bind formUpdated events to preventAutoLogout event.
+          $('body').bind('formUpdated', function(event) {
+            $(event.target).trigger('preventAutologout');
+          });
+
+          // Support for CKEditor.
+          if (typeof CKEDITOR !== 'undefined') {
+            CKEDITOR.on('instanceCreated', function(e) {
+              e.editor.on('contentDom', function() {
+                e.editor.document.on('keyup', function(event) {
+                  // Keyup event in ckeditor should prevent autologout.
+                  $(e.editor.element.$).trigger('preventAutologout');
+                }
+                );
+              });
+            });
+          }
+
+          $('body').bind('preventAutologout', function(event) {
+            // When the preventAutologout event fires
+            // we set activity to true.
+            activity = true;
+          });
+
           // On pages where the user can be logged out, set the timer to popup
           // and log them out.
           t = setTimeout(init, Drupal.settings.autologout.timeout);
@@ -19,29 +51,40 @@
       }
 
       function init() {
-        paddingTimer = setTimeout(confirmLogout, Drupal.settings.autologout.timeout_padding);
 
-        // While the countdown timer is going, lookup the remaining time. If there
-        // is more time remaining (i.e. a user is navigating in another tab), then
-        // reset the timer for opening the dialog.
-        $.ajax({
-          url : Drupal.settings.basePath + 'autologout_ajax_get_time_left',
-          dataType: 'json',
-          success: function(data) {
-            if (data.time > 0) {
-              clearTimeout(paddingTimer);
-              t = setTimeout(init, data.time);
+        if (activity) {
+          // The user has been active on the page.
+          activity = false;
+          refresh();
+        }
+        else {
+
+          // The user has not been active, ask them if they want to stay logged in
+          // and start the logout timer.
+          paddingTimer = setTimeout(confirmLogout, Drupal.settings.autologout.timeout_padding);
+
+          // While the countdown timer is going, lookup the remaining time. If there
+          // is more time remaining (i.e. a user is navigating in another tab), then
+          // reset the timer for opening the dialog.
+          $.ajax({
+            url : Drupal.settings.basePath + 'autologout_ajax_get_time_left',
+            dataType: 'json',
+            success: function(data) {
+              if (data.time > 0) {
+                clearTimeout(paddingTimer);
+                t = setTimeout(init, data.time);
+              }
+              else {
+                theDialog = dialog();
+              }
+            },
+            error: function(XMLHttpRequest, textStatus) {
+              if (XMLHttpRequest.status == 403) {
+                window.location = Drupal.settings.autologout.redirect_url;
+              }
             }
-            else {
-              theDialog = dialog();
-            }
-          },
-          error: function(XMLHttpRequest, textStatus) {
-            if (XMLHttpRequest.status == 403) {
-              window.location = Drupal.settings.autologout.redirect_url;
-            }
-          }
-        });
+          });
+        }
       }
 
       function keepAlive() {
@@ -126,6 +169,7 @@
           type: "POST",
           success: function() {
             t = setTimeout(init, Drupal.settings.autologout.timeout);
+            activity = false;
           },
           error: function(XMLHttpRequest, textStatus) {
             if (XMLHttpRequest.status == 403) {
